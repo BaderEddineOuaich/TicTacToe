41689e358f00b0f4de43207c7153c16a1f686fcc
BaderEddineOuaich <badereddineouaich@gmail.com>
2021-10-23 20:40:19 +0100

    Changed button flat style to xtd::forms::flat_style::popup

--- tictactoe/src/form1.cpp
+++ tictactoe/src/form1.cpp
@@ -1,24 +1,25 @@
 #include "form1.h"
 
 using namespace xtd::forms;
+using namespace xtd::drawing;
 using namespace tictactoe;
 
-static const xtd::drawing::size WINDOW_SIZE = { 640, 480 };
-static const xtd::drawing::size BUTTON_SIZE = { 80, 80 };
+static const size WINDOW_SIZE = { 640, 480 };
+static const size BUTTON_SIZE = { 80, 80 };
 
-static const std::array<xtd::drawing::point, 9> BUTTON_LOCATIONS =
+static const std::array<point, 9> BUTTON_LOCATIONS =
 {
-  xtd::drawing::point(100 + BUTTON_SIZE.width(),     100),
-  xtd::drawing::point(100 + BUTTON_SIZE.width() * 2, 100),
-  xtd::drawing::point(100 + BUTTON_SIZE.width() * 3, 100),
+  point(100 + BUTTON_SIZE.width(),     100),
+  point(100 + BUTTON_SIZE.width() * 2, 100),
+  point(100 + BUTTON_SIZE.width() * 3, 100),
 
-  xtd::drawing::point(100 + BUTTON_SIZE.width(),     100 + BUTTON_SIZE.height()),
-  xtd::drawing::point(100 + BUTTON_SIZE.width() * 2, 100 + BUTTON_SIZE.height()),
-  xtd::drawing::point(100 + BUTTON_SIZE.width() * 3, 100 + BUTTON_SIZE.height()),
+  point(100 + BUTTON_SIZE.width(),     100 + BUTTON_SIZE.height()),
+  point(100 + BUTTON_SIZE.width() * 2, 100 + BUTTON_SIZE.height()),
+  point(100 + BUTTON_SIZE.width() * 3, 100 + BUTTON_SIZE.height()),
 
-  xtd::drawing::point(100 + BUTTON_SIZE.width(),     100 + BUTTON_SIZE.height() * 2),
-  xtd::drawing::point(100 + BUTTON_SIZE.width() * 2, 100 + BUTTON_SIZE.height() * 2),
-  xtd::drawing::point(100 + BUTTON_SIZE.width() * 3, 100 + BUTTON_SIZE.height() * 2)
+  point(100 + BUTTON_SIZE.width(),     100 + BUTTON_SIZE.height() * 2),
+  point(100 + BUTTON_SIZE.width() * 2, 100 + BUTTON_SIZE.height() * 2),
+  point(100 + BUTTON_SIZE.width() * 3, 100 + BUTTON_SIZE.height() * 2)
 };
 
 form1::form1() {
@@ -30,54 +31,53 @@ form1::form1() {
   // Buttons
   for(std::size_t i{0u}; i < m_buttons.size(); ++i)
   {
-    m_buttons[i].reset(new xtd::forms::button()); 
+    m_buttons[i].reset(new button());
+    m_buttons[i]->flat_style(flat_style::popup);
     m_buttons[i]->parent(*this)
       .text(xtd::ustring::empty_string())
-      .font(xtd::drawing::font(xtd::drawing::font_family::generic_sans_serif(), 24, xtd::drawing::font_style::bold))
+      .font(xtd::drawing::font(font_family::generic_sans_serif(), 24, font_style::bold))
       .location(BUTTON_LOCATIONS[i])
       .size(BUTTON_SIZE)
-      .back_color(xtd::drawing::color::light_gray)
-      .fore_color(xtd::drawing::color::black)
+      .back_color(color::light_gray)
+      .fore_color(color::black)
       .click += xtd::event_handler(*this, &form1::on_button_click);
   }
  
   // Labels
-  m_player_x_score_label.reset(new xtd::forms::label());
+  m_player_x_score_label.reset(new label());
   m_player_x_score_label->parent(*this)
     .text(xtd::ustring::format("Player X: {}", m_player_x_score))
     .auto_size(true)
     .location({ 5, 5 })
-    .font(xtd::drawing::font(xtd::drawing::font_family::generic_sans_serif(), 12))
-    .fore_color(xtd::drawing::color::orange);
+    .font(xtd::drawing::font(font_family::generic_sans_serif(), 12))
+    .fore_color(color::orange);
   
-  m_player_o_score_label.reset(new xtd::forms::label());
+  m_player_o_score_label.reset(new label());
   m_player_o_score_label->parent(*this)
     .text(xtd::ustring::format("Player O: {}", m_player_o_score))
     .auto_size(true)
     .location({ size().width() - m_player_o_score_label->size().width()*2, 5 })
-    .font(xtd::drawing::font(xtd::drawing::font_family::generic_sans_serif(), 12))
-    .fore_color(xtd::drawing::color::orange);
+    .font(xtd::drawing::font(font_family::generic_sans_serif(), 12))
+    .fore_color(color::orange);
 }
 
-bool form1::there_is_a_winner()
-{
+bool form1::there_is_a_winner() {
   // | A1 | A2 | A3 |
   // |----|----|----|
   // | B1 | B2 | B3 |
   // |----|----|----|
   // | C1 | C2 | C3 |
-
-  const xtd::forms::button& A1 = *m_buttons[0];
-  const xtd::forms::button& A2 = *m_buttons[1];
-  const xtd::forms::button& A3 = *m_buttons[2];
+  const button& A1 = *m_buttons[0];
+  const button& A2 = *m_buttons[1];
+  const button& A3 = *m_buttons[2];
   
-  const xtd::forms::button& B1 = *m_buttons[3];
-  const xtd::forms::button& B2 = *m_buttons[4];
-  const xtd::forms::button& B3 = *m_buttons[5];
+  const button& B1 = *m_buttons[3];
+  const button& B2 = *m_buttons[4];
+  const button& B3 = *m_buttons[5];
   
-  const xtd::forms::button& C1 = *m_buttons[6];
-  const xtd::forms::button& C2 = *m_buttons[7];
-  const xtd::forms::button& C3 = *m_buttons[8];
+  const button& C1 = *m_buttons[6];
+  const button& C2 = *m_buttons[7];
+  const button& C3 = *m_buttons[8];
 
   // Check state where all buttons are empty (start or restart of the game)
   if (A1.text().empty() && A2.text().empty() && A3.text().empty() &&
@@ -123,17 +123,17 @@ bool form1::is_a_tie()
   // |----|----|----|
   // | C1 | C2 | C3 |
 
-  const xtd::forms::button& A1 = *m_buttons[0];
-  const xtd::forms::button& A2 = *m_buttons[1];
-  const xtd::forms::button& A3 = *m_buttons[2];
+  const button& A1 = *m_buttons[0];
+  const button& A2 = *m_buttons[1];
+  const button& A3 = *m_buttons[2];
 
-  const xtd::forms::button& B1 = *m_buttons[3];
-  const xtd::forms::button& B2 = *m_buttons[4];
-  const xtd::forms::button& B3 = *m_buttons[5];
+  const button& B1 = *m_buttons[3];
+  const button& B2 = *m_buttons[4];
+  const button& B3 = *m_buttons[5];
 
-  const xtd::forms::button& C1 = *m_buttons[6];
-  const xtd::forms::button& C2 = *m_buttons[7];
-  const xtd::forms::button& C3 = *m_buttons[8];
+  const button& C1 = *m_buttons[6];
+  const button& C2 = *m_buttons[7];
+  const button& C3 = *m_buttons[8];
 
   return (!A1.text().empty() && !A2.text().empty() && !A3.text().empty() &&
           !B1.text().empty() && !B2.text().empty() && !B3.text().empty() &&
@@ -155,7 +155,7 @@ void form1::reset()
 
 void form1::on_button_click(xtd::object& sender, const xtd::event_args&)
 {
-  xtd::forms::button& button = static_cast<xtd::forms::button&>(sender);
+  button& button = static_cast<xtd::forms::button&>(sender);
   // Ignore interactions with disabled buttons (already have X or O in them)
   if (!button.enabled()) return;
 
@@ -171,7 +171,7 @@ void form1::on_button_click(xtd::object& sender, const xtd::event_args&)
     else
       m_player_o_score_label->text(xtd::ustring::format("Player O: {}", ++m_player_o_score));
     toggle_all_buttons(false);
-    auto res = message_box::show(*this, xtd::ustring::format("Player {} is the WINNER!\nDo you want to play again ?", m_XTurn ? 'X' : 'O'), "Winner!", xtd::forms::message_box_buttons::yes_no);
+    auto res = message_box::show(*this, xtd::ustring::format("Player {} is the WINNER!\nDo you want to play again ?", m_XTurn ? 'X' : 'O'), "Winner!", message_box_buttons::yes_no);
     if(res == dialog_result::yes) {
       reset();
     }
@@ -182,7 +182,7 @@ void form1::on_button_click(xtd::object& sender, const xtd::event_args&)
   }
   else if(is_a_tie()) // draw, no winner
   {
-    auto res = message_box::show(*this, "No Winner!", "Tie!", xtd::forms::message_box_buttons::yes_no);
+    auto res = message_box::show(*this, "No Winner!", "Tie!", message_box_buttons::yes_no);
     if (res == dialog_result::yes) {
       reset();
     }
